4a8555a5f8cca2dbd8838664972cdfb408e75aba
catusax <root@coolrc.me>
2023-10-23 17:50:45 +0800

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod commands;
 pub use commands::deploy;
+pub use commands::deployment_cost;
 pub use commands::execute;
+pub use commands::execution_cost;
 pub use commands::join;
 pub use commands::split;
 pub use commands::transfer;
@@ -18,7 +20,11 @@ mod tests {
         let private_key = "APrivateKey1zkp5jS79CdFE5LbUzTvXcAd4fZLCTUVVqqgD91AApZVXJcA";
         let program_id = "credlancer_rating_survey_v2.aleo";
         let function = "submit_assessor";
-        let inputs = vec!["10000field".to_string(), "1field".to_string(), "1u64".to_string()];
+        let inputs = vec![
+            "10000field".to_string(),
+            "1field".to_string(),
+            "1u64".to_string(),
+        ];
         let record =  Some("{  owner: aleo1y3yp6kaq4tl04u06fy4k43kvcl4azlddy0fsamdj6smsa6rnxg9sk09ltz.private,  microcredits: 39349297u64.private,  _nonce: 1711234411548659851426643372350679804994150752915608367928992063815077333263group.public}");
         let fee = Some(3000u64);
         let query = Some("https://vm.aleo.org/api");
@@ -37,19 +43,6 @@ mod tests {
     }
 
     #[test]
-    fn deploy() {
-        let private_key = "APrivateKey1zkp5jS79CdFE5LbUzTvXcAd4fZLCTUVVqqgD91AApZVXJcA";
-        let program_id = "credlancer_rating_survey_v3.aleo";
-        let path = "/Users/huweixiong/code/superfreelancer/ABCDE_ZKHack/Credlancer_Update/aleo_contracts/credlancer_rating_survey/build/";
-        let record = "{  owner: aleo1y3yp6kaq4tl04u06fy4k43kvcl4azlddy0fsamdj6smsa6rnxg9sk09ltz.private,  microcredits: 39349297u64.private,  _nonce: 1711234411548659851426643372350679804994150752915608367928992063815077333263group.public}";
-        let fee = None;
-        let query = None;
-
-        let transaction = commands::deploy(private_key, program_id, path, record, fee, query);
-        println!("deploy transaction: {}", transaction.unwrap());
-    }
-
-    #[test]
     fn transfer() {
         let private_key = "APrivateKey1zkp5jS79CdFE5LbUzTvXcAd4fZLCTUVVqqgD91AApZVXJcA";
         let recipient = "aleo1y3yp6kaq4tl04u06fy4k43kvcl4azlddy0fsamdj6smsa6rnxg9sk09ltz";
@@ -64,8 +57,8 @@ mod tests {
             recipient,
             amount,
             function,
-            input_record,
-            fee_record,
+            Some(input_record),
+            Some(fee_record),
             None,
             query,
         );
@@ -83,7 +76,7 @@ mod tests {
             private_key,
             first_record,
             second_record,
-            fee_record,
+            Some(fee_record),
             None,
             None,
         );
